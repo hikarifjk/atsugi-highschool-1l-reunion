8a0334f36634fe27697a91c95628ff71935bf596
h-fujioka <h.fujioka@cec-ltd.co.jp>
2026-02-26 09:25:39 +0900

    chore: configure site/base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+export default defineConfig({
+  site: 'https://hikarifjk.github.io',
+  base: '/atsugi-highschool-1l-reunion',
+});
